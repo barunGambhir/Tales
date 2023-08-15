fcf68694ceca2f1c6220e7d918581d14445b532a
Barun Gambhir <83794264+barunGambhir@users.noreply.github.com>
2023-08-15 16:15:41 -0700

    MongoDB Connection string updated

--- backend/connection.js
+++ backend/connection.js
@@ -4,10 +4,8 @@ const connectDB = async()=>{
     try{
         //mongodb connection string
         //adding URI directly - need to configure .env file
-
-        const con = await mongoose.connect("mongodb+srv://dummyuser:Theundertaker@cluster0.jk1ty4y.mongodb.net/cmpt372?retryWrites=true&w=majority") 
-        //const con = await mongoose.connect("mongodb+srv://user2:sD2RysNG5KwJUgef@cluster0.0emyyud.mongodb.net/?retryWrites=true&w=majority")
         console.log(`MongoDb connected : ${con.connection.host}`)
+        //Removed the connection string for MongoDB
     }catch(err){
         console.log(err);
         process.exit(1);
